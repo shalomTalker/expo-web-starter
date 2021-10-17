6dde53e5db12ed80fb00c04d36a200dd885ad3f8
shalom talker <shalom604@gmail.com>
2021-10-17 19:52:34 +0300

    lawyer-2

--- src/components/MapSite/index.js
+++ src/components/MapSite/index.js
@@ -2,126 +2,128 @@ import React from 'react'
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import { keyExtractor } from '../../constants'
 import { useTheme } from '../../context/StyleContext';
+import useViewSize from '../../hooks/useViewSize';
 import { Space } from '../Spacing';
 import ListItem from './ListItem';
 
-const mapList = [{
-    title: `תפריט ראשי`,
-    items: [
-        {
-            type: 'navigate',
-            title: `דף הבית`,
-            name: "home",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'navigate',
-            title: `אודות`,
-            name: "about",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'navigate',
-            title: `תחומי עיסוק`,
-            name: "interests",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'navigate',
-            title: `צור קשר`,
-            name: "contact",
-            icon: { name: "chevron-left" }
-        },
-    ]
-},
+const mapList = [
+    //     {
+    //     title: `תפריט ראשי`,
+    //     items: [
+    //         {
+    //             type: 'navigate',
+    //             title: `דף הבית`,
+    //             name: "home",
+    //             icon: { name: "chevron-left" }
+    //         },
+    //         {
+    //             type: 'navigate',
+    //             title: `אודות`,
+    //             name: "about",
+    //             icon: { name: "chevron-left" }
+    //         },
+    //         {
+    //             type: 'navigate',
+    //             title: `תחומי עיסוק`,
+    //             name: "interests",
+    //             icon: { name: "chevron-left" }
+    //         },
+    //         {
+    //             type: 'navigate',
+    //             title: `צור קשר`,
+    //             name: "contact",
+    //             icon: { name: "chevron-left" }
+    //         },
+    //     ]
+    // },
 
-{
-    title: `שירותי המשרד`,
-    items: [
-        {
-            type: 'deep-navigate',
-            title: `עורך דין פלילי`,
-            name: "office",
-            value: "criminal",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `עורך דין צבאי`,
-            name: "office",
-            value: "military",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `עורך דין תעבורה`,
-            name: "office",
-            value: "traffic",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `עורך דין אזרחי`,
-            name: "office",
-            value: "civilLitigation",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `תביעות לשון הרע`,
-            name: "office",
-            value: "defamation",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `ליווי וייצוג נפגעי עבירה`,
-            name: "office",
-            value: "victimsCrime",
-            icon: { name: "chevron-left" }
-        },
-        {
-            type: 'deep-navigate',
-            title: `יצוג ברכישת ומכירת דירה`,
-            name: "office",
-            value: "realEstate",
-            icon: { name: "chevron-left" }
-        },
-    ]
-},
-{
-    title: `פרטי התקשרות`,
-    items: [
-        {
-            type: 'link',
-            title: `050-8347079`,
-            name: "phone",
-            icon: { name: "phone" },
-            href: 'tel:0508347079'
-        },
-        {
-            type: 'link',
-            title: `or@firmlaw.com`,
-            name: "email",
-            icon: { name: "email" },
-            href: 'mailto:or@firmlaw.com'
-        },
-        {
-            type: 'link',
-            title: `שדרות הפלי"ם 2 בניין ברוש, חיפה`,
-            name: "address",
-            icon: { name: "map-marker-radius" },
-            href: 'https://www.waze.com/ul?q=%D7%A9%D7%93%27+%D7%A4%D7%9C%D7%99%22%D7%9D+2'
-        },
-        {
-            type: 'link',
-            title: `153-50-8347079`,
-            name: "fax",
-            icon: { name: "fax" },
-            href: 'fax:+972-153-50-8347079'
-        },
-    ]
-},
+    {
+        title: `שירותי המשרד`,
+        items: [
+            {
+                type: 'deep-navigate',
+                title: `עורך דין פלילי`,
+                name: "office",
+                value: "criminal",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `עורך דין צבאי`,
+                name: "office",
+                value: "military",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `עורך דין תעבורה`,
+                name: "office",
+                value: "traffic",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `עורך דין אזרחי`,
+                name: "office",
+                value: "civilLitigation",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `תביעות לשון הרע`,
+                name: "office",
+                value: "defamation",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `ליווי וייצוג נפגעי עבירה`,
+                name: "office",
+                value: "victimsCrime",
+                icon: { name: "chevron-left" }
+            },
+            {
+                type: 'deep-navigate',
+                title: `יצוג ברכישת ומכירת דירה`,
+                name: "office",
+                value: "realEstate",
+                icon: { name: "chevron-left" }
+            },
+        ]
+    },
+    {
+        title: `פרטי התקשרות`,
+        items: [
+            {
+                type: 'link',
+                title: `050-8347079`,
+                name: "phone",
+                icon: { name: "phone" },
+                href: 'tel:0508347079'
+            },
+            {
+                type: 'link',
+                title: `or@firmlaw.com`,
+                name: "email",
+                icon: { name: "email" },
+                href: 'mailto:or@firmlaw.com'
+            },
+            {
+                type: 'link',
+                title: `שדרות הפלי"ם 2 בניין ברוש, חיפה`,
+                name: "address",
+                icon: { name: "map-marker-radius" },
+                href: 'https://www.waze.com/ul?q=%D7%A9%D7%93%27+%D7%A4%D7%9C%D7%99%22%D7%9D+2'
+            },
+            {
+                type: 'link',
+                title: `153-50-8347079`,
+                name: "fax",
+                icon: { name: "fax" },
+                href: 'fax:+972-153-50-8347079'
+            },
+        ]
+    },
 
 ]
 
@@ -129,6 +131,7 @@ const mapList = [{
 const MapSite = ({ direction }) => {
 
     const { gray, primary, c2 } = useTheme()
+    const [, , widthSize] = useViewSize()
     return (
         <View style={[styles.container, {
             flexDirection: direction, backgroundColor: c2,
@@ -148,6 +151,16 @@ const MapSite = ({ direction }) => {
                     </View>
                 )
             }
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 16 }}>
+                <Text style={{ fontSize: 20, color: primary }} >
+                    אור פרים, משרד עורכי דין מעניק שירות משפטי מלא בתחום המשפט הפלילי, משפט צבאי, תעבורה, הליכים אזרחיים וקניית ומכירת דירות. המשרד חרת על דגלו להעניק שירות אישי, יסודי, מקצועי ובלתי מתפשר לכל לקוחותיו.
+                    כל הזכויות שמורות לעורך הדין אור פרים. אין להעתיק או לעשות שימוש בכל תוכן מהאתר. תוכן האתר אינו מהווה המלצה ו/או ייעוץ משפטי.
+
+                </Text>
+            </View>
+
+
+
 
         </View>
     )
@@ -157,21 +170,24 @@ export default MapSite
 
 const styles = StyleSheet.create({
     container: {
-        // flex: 0.8,
+        flex: 1,
         justifyContent: 'space-between',
         textAlign: 'right',
         margin: 50,
         borderRadius: 8
     },
     sectionConatiner: {
+        flex: 1,
         padding: 4,
-        marginHorizontal: 16
+        marginHorizontal: 16,
     },
     flatlistContainer: {
+        flex: 1,
         marginTop: 16,
         flexDirection: 'column',
         alignItems: 'flex-end',
-        justifyContent: 'flex-end'
+        justifyContent: 'flex-start',
+        flexWrap: 'wrap'
     }
 
 })
